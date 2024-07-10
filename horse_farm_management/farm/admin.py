cb09a4f56c8f940c480e75c3c63e5172e27a3d3f
jonathasluis <jonathas_sousasgs@hotmail.com>
2024-07-10 15:43:09 -0300

    melhoria da apresentação dos dados no modo admin

--- horse_farm_management/farm/admin.py
+++ horse_farm_management/farm/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Horse, Farm, Employee
 
-admin.site.register(Horse)
-admin.site.register(Farm)
-admin.site.register(Employee)
+class HorseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'breed', 'age')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name', 'breed')
+    list_filter = ('breed',)
+
+class FarmAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'location', 'owner')
+    list_display_links = ('id', 'name')
+    search_fields =  ('id', 'name', 'location', 'owner')
+    list_filter = ('owner','location')
+    
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'position', 'farm')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name', 'position', 'farm')
+    list_filter = ('position', 'farm')
+
+admin.site.register(Horse, HorseAdmin)
+admin.site.register(Farm, FarmAdmin)
+admin.site.register(Employee, EmployeeAdmin)
--- horse_farm_management/farm/models.py
+++ horse_farm_management/farm/models.py
@@ -5,14 +5,23 @@ class Horse(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     age = models.IntegerField()
-
+    
+    def __str__(self):
+        return f"{self.name}"
+    
 class Farm(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)  # Corrigido max_length
     horses = models.ManyToManyField(Horse, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.name}"
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     position = models.CharField(max_length=100)
     farm = models.ForeignKey(Farm, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.name}"
